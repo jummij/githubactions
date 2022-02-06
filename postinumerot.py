4077eed7c3443d369ced5c53957b27e177ae3bf3
Dung <jum.pham@hotmail.com>
2022-02-06 15:39:10 +0200

    tehtävien ratkaisuja

--- postinumerot.py
+++ postinumerot.py
@@ -1,1 +1,32 @@
 # Kopioi aikaisempi ratkaisusi tänne. Lisää tarvittaessa myös muut ratkaisusi tiedostot.
+import json
+
+file = open('postcode_map_light.json')
+data = json.load(file)
+dictlist = data.items()
+
+
+city = input("Kirjoita postitoimipaikka: ")
+city = city.upper()
+
+#postal_codes = []
+
+if city in data.values():
+    keys = [k for k, v in dictlist if v == city]
+    print('Postinumerot: ' +  (str(sorted(keys))))
+else:
+    print('Tuntematon postitoimipaikka')
+
+#Testaamista varten
+
+def multiple_zipcodes(city: str)  -> bool:
+    if len(keys) > 1:
+        return True
+    else:
+        return False
+
+def district_is_in_data(city: str) -> bool:
+    if city in data:
+        return True
+    else:
+        return False
--- postinumerot_test.py
+++ postinumerot_test.py
@@ -1,1 +1,23 @@
 # Kirjoita postinumerot-moduulin testit tähän tiedostoon
+from postinumerot import multiple_zipcodes, district_is_in_data
+
+#täytyy itse kirjoittaa input...
+
+# testaa löytyykö useampi postinro
+def test_helsinki_has_multiple_zipcodes():
+    assert multiple_zipcodes('Helsinki') == True
+
+def test_kirkkonummi_has_multiple_zipcodes():
+    assert multiple_zipcodes('kirkkonummi') == True
+
+def test_vuorilahti_has_one_zipcode():
+    assert multiple_zipcodes('vuorilahti') == False
+
+# Testaa löytyykö aluetta datalistasta
+
+def test_if_helsinki_in_data():
+    assert district_is_in_data('Helsinki') == True
+
+def test_if_testikaupunki_in_data():
+    assert district_is_in_data('testikaupunki') == False
+
